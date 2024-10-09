9e0d608365c12990fd670065981bfe3f9f6ff8a8
mstsurnalyakter <surnalyms@gmail.com>
2024-10-09 15:56:30 +0600

    add: first folder

---
+++ first/index.js
@@ -0,0 +1,7 @@
+var userName = "Mst Surnaly Akter";
+var age = 22;
+var hobby = "Reading Book";
+console.log(userName);
+console.log(age);
+console.log(hobby);
+//index.ts --> tsc index.ts --> index.js --> node index.js
---
+++ first/index.ts
@@ -0,0 +1,9 @@
+let userName:string = "Mst Surnaly Akter";
+let age:number = 22;
+let hobby:string = "Reading Book";
+
+console.log(userName);
+console.log(age);
+console.log(hobby);
+
+//index.ts --> tsc index.ts --> index.js --> node index.js
