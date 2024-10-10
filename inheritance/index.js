c8eb14676e27ff328deb17e0de07004991607cef
mstsurnalyakter <surnalyms@gmail.com>
2024-10-10 22:27:35 +0600

    added inheritance

--- inheritance/index.js
+++ inheritance/index.js
@@ -36,8 +36,35 @@ var Student = /** @class */ (function (_super) {
     };
     return Student;
 }(UsersInfo));
-var student1 = new Student("hello", 32, 101);
-student1.display();
+// let student1 = new Student("hello",32,101);
+// student1.display()
 // const user1 = new UsersInfo("alex",30);
 // const user2 = new UsersInfo("ritu",32);
 // user1.display()
+var BookInfo = /** @class */ (function () {
+    function BookInfo(bookName, bookId) {
+        this.bookName = bookName,
+            this.bookId = bookId;
+    }
+    BookInfo.prototype.display = function () {
+        console.log("bookName:".concat(this.bookName, " && bookId:").concat(this.bookId));
+    };
+    return BookInfo;
+}());
+var book1 = new BookInfo("Goal", 101);
+// book1.display();
+var ImprovementBook = /** @class */ (function (_super) {
+    __extends(ImprovementBook, _super);
+    function ImprovementBook(bookName, bookId, bookType) {
+        var _this = this;
+        _this = _super.call(this, bookName, bookId) || this,
+            _this.bookType = bookType;
+        return _this;
+    }
+    ImprovementBook.prototype.display = function () {
+        console.log("BookName:".concat(this.bookName, " && bookId:").concat(this.bookId, " && bookType:").concat(this.bookType));
+    };
+    return ImprovementBook;
+}(BookInfo));
+var improvementBook1 = new ImprovementBook("mim", 101, "improve");
+improvementBook1.display();
--- inheritance/index.ts
+++ inheritance/index.ts
@@ -24,11 +24,45 @@ class Student extends UsersInfo{
     }
 }
 
-let student1 = new Student("hello",32,101);
-student1.display()
+// let student1 = new Student("hello",32,101);
+// student1.display()
 
 
 // const user1 = new UsersInfo("alex",30);
 // const user2 = new UsersInfo("ritu",32);
 
 // user1.display()
+
+
+class BookInfo{
+    bookName:string;
+    bookId:number;
+
+    constructor(bookName:string,bookId:number){
+        this.bookName = bookName,
+        this.bookId = bookId
+    }
+
+    display():void{
+        console.log(`bookName:${this.bookName} && bookId:${this.bookId}`);
+    }
+}
+
+const book1 = new BookInfo("Goal",101);
+// book1.display();
+
+class ImprovementBook extends BookInfo{
+    bookType:string;
+
+    constructor(bookName:string,bookId:number,bookType:string){
+        super(bookName,bookId),
+        this.bookType= bookType
+    }
+    display(): void {
+        console.log(`BookName:${this.bookName} && bookId:${this.bookId} && bookType:${this.bookType}`);
+    }
+}
+
+const improvementBook1 = new ImprovementBook("mim",101,"improve");
+
+improvementBook1.display()
